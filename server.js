96503bdb595e32bf84c0586965369b6e5ae25e84
jean1903 <jeancarlosprogresso2002@gmail.com>
2026-03-18 23:31:34 -0300

    Remove path dependency and update static file serving

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(__dirname));
 const players = {};
 const ZONES = {
   reception: { x: 50, y: 50, width: 300, height: 200 },
